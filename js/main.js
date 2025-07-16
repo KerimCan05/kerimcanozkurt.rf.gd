8381707f1cb38b6cb93c924e43ea764858282225
KerimCan05 <kerim_can05@proton.me>
2025-07-16 10:42:32 +0300

    .

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-// Set your local timezone
 const myTimezone = "Europe/Istanbul";
 
-// Function to update your local time
 function updateLocalTime() {
     const now = new Date();
     const options = {
@@ -16,10 +14,8 @@ function updateLocalTime() {
     document.getElementById('timeDisplay').textContent = `${timeString}`;
 }
 
-// Update time every second
 setInterval(updateLocalTime, 1000);
 
-// Initialize
 window.addEventListener('load', () => {
     updateLocalTime();
 });
